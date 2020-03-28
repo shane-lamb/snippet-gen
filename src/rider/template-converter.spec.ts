78f3daa1ee08b1c5d120ba4aa985d824fa66db0a
Shane Lamb <shanelamb1@gmail.com>
2020-03-28 20:23:28 +1000

    improved rider template converter

--- src/rider/template-converter.spec.ts
+++ src/rider/template-converter.spec.ts
@@ -5,32 +5,6 @@ import { XMLBuilder } from 'xmlbuilder2/lib/interfaces'
 import { Template } from '../types'
 import { templateToRows } from './template-converter'
 
-const sample = `
-<wpf:ResourceDictionary xml:space="preserve" xmlns:x="http://schemas.microsoft.com/winfx/2006/xaml" xmlns:s="clr-namespace:System;assembly=mscorlib" xmlns:ss="urn:shemas-jetbrains-com:settings-storage-xaml" xmlns:wpf="http://schemas.microsoft.com/winfx/2006/xaml/presentation">
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/@KeyIndexDefined">True</s:Boolean>
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Applicability/=Live/@EntryIndexedValue">True</s:Boolean>
-	<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Description/@EntryValue">async test method</s:String>
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Field/=NAME/@KeyIndexDefined">True</s:Boolean>
-	<s:Int64 x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Field/=NAME/Order/@EntryValue">0</s:Int64>
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Reformat/@EntryValue">True</s:Boolean>
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Scope/=C3001E7C0DA78E4487072B7E050D86C5/@KeyIndexDefined">True</s:Boolean>
-	<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Scope/=C3001E7C0DA78E4487072B7E050D86C5/CustomProperties/=minimumLanguageVersion/@EntryIndexedValue">2.0</s:String>
-	<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Scope/=C3001E7C0DA78E4487072B7E050D86C5/Type/@EntryValue">InCSharpFile</s:String>
-	<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Shortcut/@EntryValue">tma</s:String>
-	<s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/ShortenQualifiedReferences/@EntryValue">True</s:Boolean>
-	<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=04FCFC465F439945B8515EA72E5FAA28/Text/@EntryValue">[Fact]
-public async void $NAME$()
-{
-    $END$
-}</s:String>
-`
-
-function assertEquals(expected: string, actual: XMLBuilder[]) {
-    const expectedStrings = expected.split('\n').map(x => x.trim()).filter(Boolean)
-    const actualStrings = actual.map(x => x.toString())
-    expect(actualStrings).toIncludeSameMembers(expectedStrings)
-}
-
 describe('rider template converter', () => {
     beforeEach(() => {
         randomBytes.mockReset()
@@ -45,7 +19,7 @@ describe('rider template converter', () => {
             description: 'Assert.Equal()',
             shortcut: 'ae',
             settings: {
-                'contexts': ['InCSharpStatement']
+                'context': 'InCSharpStatement'
             }
         }
 
@@ -64,4 +38,25 @@ describe('rider template converter', () => {
             <s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Scope/=2C285F182AC98D44B0B4F29D4D2149EC/Type/@EntryValue">InCSharpStatement</s:String>
         `, result)
     })
+
+    it('uses global context when missing', () => {
+        const input: Template = {
+            template: '',
+            description: '',
+            shortcut: '',
+            settings: {}
+        }
+
+        const result = templateToRows(input)
+
+        expect(result.map(x => x.toString())).toContain(
+            '<s:String x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Scope/=2C285F182AC98D44B0B4F29D4D2149EC/Type/@EntryValue">InCSharpFile</s:String>'
+        )
+    })
 })
+
+function assertEquals(expected: string, actual: XMLBuilder[]) {
+    const expectedStrings = expected.split('\n').map(x => x.trim()).filter(Boolean)
+    const actualStrings = actual.map(x => x.toString())
+    expect(actualStrings).toIncludeSameMembers(expectedStrings)
+}
--- src/rider/template-converter.ts
+++ src/rider/template-converter.ts
@@ -16,11 +16,7 @@ export function templateToRows(template: Template): XMLBuilder[] {
         fragment().ele('s:Boolean', {'x:Key': baseKey + 'Reformat/@EntryValue'}).txt('True'),
         fragment().ele('s:Boolean', {'x:Key': scopeBaseKey + '@KeyIndexDefined'}).txt('True'),
         fragment().ele('s:String', {'x:Key': scopeBaseKey + 'CustomProperties/=minimumLanguageVersion/@EntryIndexedValue'}).txt('2.0'),
-        fragment().ele('s:String', {'x:Key': scopeBaseKey + 'Type/@EntryValue'}).txt(template.settings['contexts'][0]),
-        // <s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Field/=ACTUAL/@KeyIndexDefined">True</s:Boolean>
-        // <s:Int64 x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Field/=ACTUAL/Order/@EntryValue">1</s:Int64>
-        // <s:Boolean x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Field/=EXPECTED/@KeyIndexDefined">True</s:Boolean>
-        // <s:Int64 x:Key="/Default/PatternsAndTemplates/LiveTemplates/Template/=483DA23FE5010B45942DD5877D9DDE2D/Field/=EXPECTED/Order/@EntryValue">0</s:Int64>
+        fragment().ele('s:String', {'x:Key': scopeBaseKey + 'Type/@EntryValue'}).txt(template.settings['context'] || 'InCSharpFile'),
     ];
 }
 
