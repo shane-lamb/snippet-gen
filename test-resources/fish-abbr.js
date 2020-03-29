abaf0df38c16fd4a699cc86cdb7a23542750e5b6
Shane Lamb <shanelamb1@gmail.com>
2020-03-29 17:42:43 +1000

    added fish abbreviations example

--- test-resources/fish-abbr.js
+++ test-resources/fish-abbr.js
@@ -1,3 +1,13 @@
+const gpl = {
+    o: {
+        template: ' origin',
+        children: {
+            m: ' master'
+        }
+    },
+    u: ' upstream'
+}
+
 module.exports = {
     targetFile: './abbr.fish',
     adapter: 'fish-abbr',
@@ -16,13 +26,63 @@ module.exports = {
                         d: ' -D'
                     }
                 },
+                ch: {
+                    template: ' checkout',
+                    children: {
+                        b: ' -b',
+                        m: ' master'
+                    }
+                },
                 d: {
                     template: ' diff',
                     children: {
                         c: ' --cached'
                     }
                 },
-                k: ' commit -m',
+                k: {
+                    template: ' commit',
+                    abstract: true,
+                    children: {
+                        '': ' -m',
+                        a: ' --amend'
+                    }
+                },
+                l: ' log',
+                pl: {
+                    template: ' pull',
+                    children: {
+                        r: {
+                            template: ' -r',
+                            children: gpl
+                        },
+                        ...gpl
+                    }
+                },
+                ps: {
+                    template: ' push',
+                    children: {
+                        f: ' -f'
+                    }
+                },
+                r: ' reset',
+                sh: {
+                    template: ' stash',
+                    children: {
+                        a: ' apply'
+                    }
+                }
+            }
+        },
+        n: {
+            template: 'npm',
+            children: {
+                i: {
+                    template: ' install',
+                    children: {
+                        s: ' --save',
+                        sd: ' --save-dev'
+                    }
+                }
             }
         }
     }
